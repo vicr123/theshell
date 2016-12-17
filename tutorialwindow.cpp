75f03a448881ecf862dd94f418f378bf35003910
Victor Tran <vicr12345@gmail.com>
2016-12-18 00:25:04 +1100

    Tutorial window does not appear in pager

--- tutorialwindow.cpp
+++ tutorialwindow.cpp
@@ -1,6 +1,8 @@
 #include "tutorialwindow.h"
 #include "ui_tutorialwindow.h"
 
+extern void sendMessageToRootWindow(const char* message, Window window, long data0 = 0, long data1 = 0, long data2 = 0, long data3 = 0, long data4 = 0);
+
 TutorialWindow::TutorialWindow(bool doSettings, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::TutorialWindow)
@@ -8,11 +10,16 @@ TutorialWindow::TutorialWindow(bool doSettings, QWidget *parent) :
     ui->setupUi(this);
 
     //this->setAttribute(Qt::WA_TransparentForMouseEvents);
-    this->setWindowFlags(Qt::WindowStaysOnTopHint);
+    this->setWindowFlags(Qt::WindowStaysOnTopHint | Qt::WindowDoesNotAcceptFocus);\
     this->setFocusPolicy(Qt::NoFocus);
     doMask();
 
     settings.beginGroup("tutorial");
+
+    Atom DesktopWindowTypeAtom;
+    DesktopWindowTypeAtom = XInternAtom(QX11Info::display(), "_NET_WM_WINDOW_TYPE_UTILITY", False);
+    XChangeProperty(QX11Info::display(), this->winId(), XInternAtom(QX11Info::display(), "_NET_WM_WINDOW_TYPE", False),
+                     XA_ATOM, 32, PropModeReplace, (unsigned char*) &DesktopWindowTypeAtom, 1); //Change Window Type
 
     unsigned long desktop = 0xFFFFFFFF;
     qDebug() << XChangeProperty(QX11Info::display(), this->winId(), XInternAtom(QX11Info::display(), "_NET_WM_DESKTOP", False),
@@ -20,6 +27,8 @@ TutorialWindow::TutorialWindow(bool doSettings, QWidget *parent) :
 
     this->showFullScreen();
     this->doSettings = doSettings;
+
+    //sendMessageToRootWindow("_NET_WM_STATE", this->winId(), 1, XInternAtom(QX11Info::display(), "_NET_WM_STATE_SKIP_PAGER", False), XInternAtom(QX11Info::display(), "_NET_WM_STATE_SKIP_TASKBAR", False), 1);
 }
 
 TutorialWindow::~TutorialWindow()
