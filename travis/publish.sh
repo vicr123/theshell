7dde18f1f3bd1eee3fd543f5bbf0f5923611bf06
Victor Tran <vicr12345@gmail.com>
2018-08-09 18:17:07 +1000

    Update Travis script

--- travis/publish.sh
+++ travis/publish.sh
@@ -4,4 +4,5 @@ git rm theshell-blueprint*.pkg.tar.xz
 cp /build/theshell-blueprint/theshell-blueprint*.pkg.tar.xz ./
 repo-add theappsbp.db.tar.gz theshell-blueprint*.pkg.tar.xz
 git add .
+git commit -m "Update theShell Blueprint"
 git push https://vicr123:$GITHUB_TOKEN@github.com/vicr123/vicr123.github.io.git
