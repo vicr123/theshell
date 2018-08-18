8ff2168750dd96534a36c6281e6a8800d47d2f51
Victor Tran <vicr12345@gmail.com>
2018-08-18 22:59:09 +1000

    Push to new repo

--- travis/publish.sh
+++ travis/publish.sh
@@ -1,10 +1,3 @@
-git clone https://github.com/vicr123/vicr123.github.io.git MainSite
-cd MainSite/repo/arch/x86_64
-git rm theshell-blueprint*.pkg.tar.xz
-cp /build/theshell-blueprint/theshell-blueprint*.pkg.tar.xz ./
-repo-add theappsbp.db.tar.gz theshell-blueprint*.pkg.tar.xz
-git add .
-git config user.email $GIT_EMAIL
-git config user.name $GIT_NAME
-git commit -m "Update theShell Blueprint"
-git push https://vicr123:$GITHUB_TOKEN@github.com/vicr123/vicr123.github.io.git
+for i in /build/theshell-blueprint/theshell-blueprint*.pkg.tar.xz; do
+    curl -X POST --data-binary @$i -H "Filename: $(basename $i)" -H "Repository: theappsbp" "https://$REPO_USERNAME:$REPO_PASSWORD@packages.vicr123.com/push"
+done
