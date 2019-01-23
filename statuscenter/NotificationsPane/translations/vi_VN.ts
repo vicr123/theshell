9d9828901d21f3b176e3255358928384bb318b48
Victor Tran <vicr12345@gmail.com>
2019-01-23 11:54:53 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 91.3% (21 of 23 strings)
    
    Translation: theShell/theShell Notifications
    Translate-URL: http://translate.vicr123.com/projects/theshell/theshell-notifications/vi/

--- statuscenter/NotificationsPane/translations/vi_VN.ts
+++ statuscenter/NotificationsPane/translations/vi_VN.ts
@@ -1,14 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>JobServer</name>
     <message numerus="yes">
         <source>%n jobs running</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n việc đang chạy</numerusform>
+        </translation></message>
 </context>
 <context>
     <name>JobViewWidget</name>
@@ -21,92 +19,84 @@
     <name>NotificationAppGroup</name>
     <message>
         <source>Collapse Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Thu gọn thông báo</translation>
     </message>
     <message numerus="yes">
         <source>+%n notifications collapsed</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>+%n thông báo thu gọn</numerusform>
+        </translation></message>
 </context>
 <context>
     <name>NotificationPanel</name>
     <message numerus="yes">
         <source>%n d</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n ng</numerusform>
+        </translation></message>
     <message numerus="yes">
         <source>%n hr</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n g</numerusform>
+        </translation></message>
     <message numerus="yes">
         <source>%n min</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n ph</numerusform>
+        </translation></message>
     <message>
         <source>just now</source>
-        <translation type="unfinished"></translation>
+        <translation>vừa báo</translation>
     </message>
 </context>
 <context>
     <name>NotificationPopup</name>
     <message>
         <source>Dismiss</source>
-        <translation type="unfinished"></translation>
+        <translation>Bỏ qua</translation>
     </message>
 </context>
 <context>
     <name>NotificationsWidget</name>
     <message>
         <source>No notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thông báo</translation>
     </message>
     <message>
         <source>There&apos;s nothing to see at the moment. Don&apos;t worry, we&apos;ll be sure to tell you as soon as something pops up.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hiện tại, không có gì để xem. Đừng lo, chúng tôi chắc chắn báo với bạn khi điều gì hiển thị.</translation>
     </message>
     <message>
         <source>Clear All</source>
-        <translation type="unfinished"></translation>
+        <translation>Bỏ tất cả</translation>
     </message>
     <message>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Thông báo</translation>
     </message>
     <message>
         <source>Quiet Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Chế độ im</translation>
     </message>
     <message>
         <source>Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>Âm</translation>
     </message>
     <message>
         <source>Critical Only</source>
-        <translation type="unfinished"></translation>
+        <translation>Chỉ quan trọng</translation>
     </message>
     <message>
         <source>No Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Chặn thông báo</translation>
     </message>
     <message>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>Im lặng</translation>
     </message>
     <message>
         <source>Fore&amp;ver</source>
-        <translation type="unfinished"></translation>
+        <translation>Mãi mãi</translation>
     </message>
     <message>
         <source>Turn off in</source>
-        <translation type="unfinished"></translation>
+        <translation>Tắt sau</translation>
     </message>
     <message>
         <source>HH:mm</source>
@@ -114,13 +104,11 @@
     </message>
     <message>
         <source>Turn off at</source>
-        <translation type="unfinished"></translation>
+        <translation>Tắt tại</translation>
     </message>
     <message numerus="yes">
         <source>%n notification(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n thông báo</numerusform>
+        </translation></message>
 </context>
 </TS>
