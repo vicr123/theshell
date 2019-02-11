2755135abde0c8002fbec032a04c5e89f3e58566
Victor Tran <vicr12345@gmail.com>
2019-02-11 07:48:20 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (66 of 66 strings)
    
    Translation: theShell/theShell KDE Connect Integration
    Translate-URL: http://translate.vicr123.com/projects/theshell/theshell-kde-connect/vi/

--- statuscenter/KDEConnectPane/translations/vi_VN.ts
+++ statuscenter/KDEConnectPane/translations/vi_VN.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>KdeConnectDevicesModel</name>
     <message>
         <location filename="../kdeconnect/kdeconnectdevicesmodel.cpp" line="104"/>
         <source>Tablet</source>
-        <translation type="unfinished"></translation>
+        <translation>Máy tính bảng</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectdevicesmodel.cpp" line="106"/>
         <source>Computer</source>
-        <translation type="unfinished"></translation>
+        <translation>Máy tính</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectdevicesmodel.cpp" line="108"/>
         <source>Phone</source>
-        <translation type="unfinished"></translation>
+        <translation>Điện thoại</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectdevicesmodel.cpp" line="110"/>
         <source>Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Thiết bị</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectdevicesmodel.cpp" line="114"/>
         <source>Ready to pair</source>
-        <translation type="unfinished"></translation>
+        <translation>Sẵn sàng ghép nối</translation>
     </message>
 </context>
 <context>
@@ -35,317 +35,315 @@
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="80"/>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="66"/>
         <source>KDE Connect</source>
-        <translation type="unfinished"></translation>
+        <translation>KDE Connect</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="206"/>
         <source>No Device Selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Chưa chọn thiết bị</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="216"/>
         <source>If you have an Android device, you can install the KDE Connect Android app on Google Play or on F-Droid.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nếu bạn có thiết bị Android, bạn có thể cài đặt ứng dụng KDE Connect trên CH Google Play hoặc F-Droid</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="242"/>
         <source>Google Play and the Google Play logo are trademarks of Google LLC.</source>
-        <translation type="unfinished"></translation>
+        <translation>Google Play và biểu trưng Google Play là các nhãn hiệu của Google LLC.</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="320"/>
         <source>Encryption Info</source>
-        <translation type="unfinished"></translation>
+        <translation>Thông tin mã hóa</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="343"/>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="703"/>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="929"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="431"/>
         <source>Available Actions</source>
-        <translation type="unfinished"></translation>
+        <translation>Hoạt động có sẵn</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="441"/>
         <source>Ping Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Ping thiết bị</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="444"/>
         <source>Sends a notification to the device to see if KDE Connect is working properly</source>
-        <translation type="unfinished"></translation>
+        <translation>Gửi thông báo đến thiết bị để kiểm tra kết nối KDE Connect</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="451"/>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="186"/>
         <source>Locate Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Tìm kiếm thiết bị</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="454"/>
         <source>If you&apos;ve lost your phone, you can locate your device by making it play a sound</source>
-        <translation type="unfinished"></translation>
+        <translation>Nếu bạn mất thiết bị, bạn có thể tìm được bởi phát âm thanh trên thiết bị</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="461"/>
         <source>Send File</source>
-        <translation type="unfinished"></translation>
+        <translation>Gửi tệp</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="464"/>
         <source>Send a file to the other device</source>
-        <translation type="unfinished"></translation>
+        <translation>Gửi tệp đến thiết bị</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="471"/>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="1074"/>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="425"/>
         <source>Send SMS</source>
-        <translation type="unfinished"></translation>
+        <translation>Gửi SMS</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="474"/>
         <source>Send a text message</source>
-        <translation type="unfinished"></translation>
+        <translation>Gửi tin nhắn</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="511"/>
         <source>Device Unreachable</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể kết nối với thiết bị</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="521"/>
         <source>Can&apos;t get to this device. Is it connected to the same network and powered on?</source>
-        <translation type="unfinished"></translation>
+        <translation>Chúng tôi không có thể kết nối với thiết bị này. Bạn có chắc chắn nó kết nối với mạng giống và bật không?</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="567"/>
         <source>Device Unpaired</source>
-        <translation type="unfinished"></translation>
+        <translation>Gỡ ghép nối với thiết bị</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="577"/>
         <source>You&apos;ll need to pair this device before you can use it with KDE Connect</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn sẽ cần ghép nối lại trước khi bạn dùng với KDE connect được</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="602"/>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="222"/>
         <source>Pair</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghép Nối</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="745"/>
         <source>Plugins</source>
-        <translation type="unfinished"></translation>
+        <translation>Bộ cắm</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="752"/>
         <source>Battery Monitor</source>
-        <translation type="unfinished"></translation>
+        <translation>Bộ theo dõi pin</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="759"/>
         <source>Clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Bảng tạm</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="766"/>
         <source>Inhibit screensaver</source>
-        <translation type="unfinished"></translation>
+        <translation>Tránh bộ bảo vệ màn hình</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="773"/>
         <source>Multimedia control receiver</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhận điều khiển phương tiện</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="780"/>
         <source>Pause media during calls</source>
-        <translation type="unfinished"></translation>
+        <translation>Tạm dừng phương tiện khi cuộc gọi</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="787"/>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="177"/>
         <source>Ping</source>
-        <translation type="unfinished"></translation>
+        <translation>Ping</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="794"/>
         <source>Receive Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhận thông báo</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="801"/>
         <source>Remote filesystem browser</source>
-        <translation type="unfinished"></translation>
+        <translation>Duyệt hệ thống tệp xa</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="808"/>
         <source>Remote keyboard from the desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Bàn phím từ đây</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="815"/>
         <source>Ring my phone</source>
-        <translation type="unfinished"></translation>
+        <translation>Gọi điện thoại của tôi</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="822"/>
         <source>Run commands</source>
-        <translation type="unfinished"></translation>
+        <translation>Chạy lệnh</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="829"/>
         <source>Send notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Gửi thông báo</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="836"/>
         <source>Send and receive</source>
-        <translation type="unfinished"></translation>
+        <translation>Gửi và nhận</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="843"/>
         <source>Telephony integration</source>
-        <translation type="unfinished"></translation>
+        <translation>Tích hợp cuộc gọi điện thoại</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="850"/>
         <source>Virtual input</source>
-        <translation type="unfinished"></translation>
+        <translation>Bàn nhập ảo</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="1113"/>
         <source>Send to</source>
-        <translation type="unfinished"></translation>
+        <translation>Gửi đến</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="1123"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Tin nhắn</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="1175"/>
         <source>Send</source>
-        <translation type="unfinished"></translation>
+        <translation>Gửi</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="1210"/>
         <source>KDE Connect not running</source>
-        <translation type="unfinished"></translation>
+        <translation>KDE Connect chưa khởi động</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="1263"/>
         <source>Start KDE Connect</source>
-        <translation type="unfinished"></translation>
+        <translation>Khởi động KDE Connect</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.ui" line="1289"/>
         <source>If you&apos;re still getting this error, KDE Connect might not be installed properly on this device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nếu bạn vẫn thấy lỗi này, KDE Connect có thể chưa cài đặt thành công trên thiết bị này.</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="82"/>
         <source>This device is called &lt;b&gt;%1&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Thiết bị này gọi &lt;b&gt;%1&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="93"/>
         <source>KDE Connect is not running</source>
-        <translation type="unfinished"></translation>
+        <translation>KDE Connect chưa khởi động</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="99"/>
         <source>Install KDE Connect for KDE Connect integration with theShell</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt KDE Connect để tích hợp KDE Connect với theShell</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="102"/>
         <source>You can attempt to start KDE Connect again</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn có thể thử khởi động KDE Connect lại</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="143"/>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="204"/>
         <source>Unpair Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Gỡ ghép nối</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="146"/>
         <source>Pair Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghép nối</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="178"/>
         <source>Ping sent to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ping được gửi đến %1</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="186"/>
         <source>Your device will play its ringtone at full volume. To stop it ringing, tap the button on the device&apos;s screen.</source>
-        <translation type="unfinished"></translation>
+        <translation>Thiết bị của bạn sẽ phát chuông gọi ở mức âm lượng đầy đủ. Đẻ dừng, nhấn nút trên màn hình thiết bị.</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="192"/>
         <source>Locate</source>
-        <translation type="unfinished"></translation>
+        <translation>Tìm kiếm</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="193"/>
         <source>The request to play a sound has been sent to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Yêu cầu phát âm thanh đã bị gửi đến %1</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="204"/>
         <source>Do you want to unpair this device?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn có muốn gỡ ghép nối thiết bị này</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="210"/>
         <source>Unpair</source>
-        <translation type="unfinished"></translation>
+        <translation>Gỡ ghép nối</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="211"/>
         <source>%1 has been unpaired from this device. To use KDE Connect between these devices again, you&apos;ll need to pair them.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghép nối của %1 đã bị gỡ. Để dùng KDE Connect lại với thiết bị này, bạn cần ghép nối lại.</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="223"/>
         <source>A pairing request has been sent to %1. To complete pairing, you&apos;ll need to respond on %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Yêu cầu ghép nối đã bị gửi đến %1. Để hoàn thành việc ghép nối, bạn cần trả lời trên %1.</translation>
     </message>
     <message numerus="yes">
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="406"/>
         <source>Sending as %n messages</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Gửi thành %n tin nhắn</numerusform>
+        </translation></message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="426"/>
         <source>Your SMS has been sent to your device and is on its way!</source>
-        <translation type="unfinished"></translation>
+        <translation>Tin nhắn SMS đã bị gửi đến thiết bị của bạn, và trên đường!</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="444"/>
         <source>Encryption Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Thông tin mã hóa</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="465"/>
         <source>Rename Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Đổi tên thiết bị</translation>
     </message>
     <message>
         <location filename="../kdeconnect/kdeconnectwidget.cpp" line="465"/>
         <source>What do you want to call this device?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn muốn gọi thiết bị này là gì?</translation>
     </message>
 </context>
 </TS>
