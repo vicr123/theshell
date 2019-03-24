23b03ec0288960da502fc54afc5af307778b143d
Victor Tran <vicr12345@gmail.com>
2019-03-25 00:41:26 +1100

    Add translations

---
+++ statuscenter/CoreSettings/translations/au_AU.ts
@@ -0,0 +1,212 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US">
+<context>
+    <name>GesturePane</name>
+    <message>
+        <location filename="../Input/gesturepane.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/gesturepane.ui" line="40"/>
+        <source>Gestures</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/gesturepane.ui" line="63"/>
+        <source>These gestures work on touch screens.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/gesturepane.ui" line="143"/>
+        <source>Touch Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/gesturepane.ui" line="150"/>
+        <source>Optimise theShell for touch screen input by making buttons bigger. &lt;b&gt;Requires a theShell restart&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/gesturepane.ui" line="166"/>
+        <source>TOUCH MODE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/gesturepane.ui" line="244"/>
+        <source>Swipe to open Gateway</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/gesturepane.ui" line="251"/>
+        <source>SwipeOpenGatewaySwitch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/gesturepane.ui" line="277"/>
+        <source>Swipe from the left side of the screen to open the Gateway</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/gesturepane.ui" line="293"/>
+        <source>SWIPE TO OPEN GATEWAY</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InputPane</name>
+    <message>
+        <location filename="../Input/inputpane.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/inputpane.ui" line="95"/>
+        <location filename="../Input/inputpane.cpp" line="50"/>
+        <source>Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/inputpane.ui" line="130"/>
+        <source>Keyboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/inputpane.ui" line="138"/>
+        <source>Mouse and Touchpad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/inputpane.ui" line="146"/>
+        <source>Gestures</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>KeyboardPane</name>
+    <message>
+        <location filename="../Input/keyboardpane.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/keyboardpane.ui" line="62"/>
+        <source>Keyboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/keyboardpane.ui" line="141"/>
+        <source>LAYOUTS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/keyboardpane.ui" line="148"/>
+        <source>Select the keyboard layouts you wish to use. You can switch between layouts using the keyboard button on the Bar, or by using the keyboard combination SUPER+ENTER.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/keyboardpane.ui" line="220"/>
+        <source>Use Super Key to open Gateway</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/keyboardpane.ui" line="246"/>
+        <source>SYSTEM SHORTCUTS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/keyboardpane.ui" line="253"/>
+        <source>If you&apos;d like to use the Super Key for a different app, turn this off so theShell won&apos;t take control of it. &lt;b&gt;Requires a theShell restart&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/keyboardpane.ui" line="324"/>
+        <source>Add Keyboard Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/keyboardpane.cpp" line="106"/>
+        <source>New Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/keyboardpane.cpp" line="174"/>
+        <source>For %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/keyboardpane.cpp" line="175"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MousePane</name>
+    <message>
+        <location filename="../Input/mousepane.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/mousepane.ui" line="40"/>
+        <source>Mouse and Touchpad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/mousepane.ui" line="173"/>
+        <source>Primary Button</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/mousepane.ui" line="193"/>
+        <source>Tap to click</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/mousepane.ui" line="239"/>
+        <source>Pointer Speed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/mousepane.ui" line="265"/>
+        <source>MOVEMENT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/mousepane.ui" line="304"/>
+        <source>Natural Mouse Scrolling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/mousepane.ui" line="318"/>
+        <source>Natural Touchpad Scrolling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/mousepane.ui" line="351"/>
+        <source>SCROLLING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/mousepane.ui" line="358"/>
+        <source>Natural Scrolling moves content in the direction of your finger instead of the scrollbar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/mousepane.ui" line="131"/>
+        <source>Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/mousepane.ui" line="144"/>
+        <source>Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Input/mousepane.ui" line="186"/>
+        <source>CLICKING</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
