263ff2e384e66ffa3b029c61ac94bacebdfd38e6
Victor Tran <vicr12345@gmail.com>
2019-03-30 10:52:08 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: theShell/theShell Display
    Translate-URL: http://translate.vicr123.com/projects/theshell/theshell-display/vi/

--- statuscenter/DisplayPane/translations/vi_VN.ts
+++ statuscenter/DisplayPane/translations/vi_VN.ts
@@ -1,37 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>DisplayConfigurationWidget</name>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="63"/>
         <source>Display Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Tên màn hình</translation>
     </message>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="79"/>
         <source>Set as Default monitor</source>
-        <translation type="unfinished"></translation>
+        <translation>Đặt thành màn hình mặc định</translation>
     </message>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="108"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Xong</translation>
     </message>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="157"/>
         <source>Resolution</source>
-        <translation type="unfinished"></translation>
+        <translation>Độ phân giải</translation>
     </message>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="164"/>
         <source>Power</source>
-        <translation type="unfinished"></translation>
+        <translation>Bật</translation>
     </message>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="188"/>
         <source>Refresh Rate</source>
-        <translation type="unfinished"></translation>
+        <translation>Tốc độ làm tươi</translation>
     </message>
 </context>
 <context>
@@ -40,97 +40,97 @@
         <location filename="../displaypane.ui" line="40"/>
         <location filename="../displaypane.cpp" line="115"/>
         <source>Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Màn hình</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="135"/>
         <source>Screen Scaling</source>
-        <translation type="unfinished"></translation>
+        <translation>Co giãn màn hình</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="145"/>
         <source>If apps and buttons look too small on your device, you can scale them up to improve usability.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nếu ứng dụng và nút bấm trên thiết bị của bạn nhỏ quá, bạn có thể phóng to lên để nâng cao khả năng sử dụng.</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="161"/>
         <source>SCALING</source>
-        <translation type="unfinished"></translation>
+        <translation>CO GIÃN</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="189"/>
         <source>&amp;100% (96 dpi)</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;100% (96 dpi)</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="196"/>
         <source>1&amp;50% (144 dpi)</source>
-        <translation type="unfinished"></translation>
+        <translation>1&amp;50% (144 dpi)</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="203"/>
         <source>&amp;200% (192 dpi)</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;200% (192 dpi)</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="210"/>
         <source>&amp;300% (288 dpi)</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;300% (288 dpi)</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="217"/>
         <source>DPI settings will be applied at next login.</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt DPI sẽ bị đặt khi bạn đăng nhập lại.</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="262"/>
         <source>If the start time is later than the end time, Redshift will be activated until the end time on the next day.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nếu thời gian bắt đầu sau thời gian kết thúc, Redshift sẽ bị bật đén khi thời gian kết thúc ngày tiếp.</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="294"/>
         <source>Scheduled</source>
-        <translation type="unfinished"></translation>
+        <translation>Lịch biểu</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="318"/>
         <source>End Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Thời gian kết thúc</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="349"/>
         <source>We&apos;ll send your current location to https://sunrise-sunset.org/ to determine sunlight times if you enable this option.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chúng tôi sẽ gửi vị trí của bạn đến https://sunrise-sunset.org/ để xác định giờ hoàng hôn và giờ bình minh nếu bạn bật tùy chọn này.</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="377"/>
         <source>Use Sunlight Cycle</source>
-        <translation type="unfinished"></translation>
+        <translation>Sử dụng thời gian ánh sáng mặt trời</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="403"/>
         <source>Redshift Intensity</source>
-        <translation type="unfinished"></translation>
+        <translation>Cường độ Redshift</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="416"/>
         <source>REDSHIFT</source>
-        <translation type="unfinished"></translation>
+        <translation>REDSHIFT</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="423"/>
         <source>Start Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Thời gian bắt đầu</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="430"/>
         <source>Redshift tints the display a warmer shade at night, which may help you get a better night&apos;s sleep.</source>
-        <translation type="unfinished"></translation>
+        <translation>Redshift tông màu màn hình thành màu ấm hơn vào ban đêm, nó có thể giúp bạn ngủ ngon hơn.</translation>
     </message>
     <message>
         <location filename="../displaypane.cpp" line="89"/>
         <source>Redshift</source>
-        <translation type="unfinished"></translation>
+        <translation>Redshift</translation>
     </message>
 </context>
 <context>
@@ -138,27 +138,27 @@
     <message>
         <location filename="../displaypositionwidget.ui" line="26"/>
         <source>SCREENS</source>
-        <translation type="unfinished"></translation>
+        <translation>MÀN HÌNH</translation>
     </message>
     <message>
         <location filename="../displaypositionwidget.ui" line="33"/>
         <source>Select a screen to configure it, or drag it around to reposition it. To duplicate displays, drag them on top of each other and set them to have the same resolution.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chọn màn hình để cấu hình, hoặc kéo xung quanh để đặt vị trí. Để nhân đôi màn hình, kéo chúng lên nhau, và đặt để có cùng độ phân giải.</translation>
     </message>
     <message>
         <location filename="../displaypositionwidget.ui" line="78"/>
         <source>Set</source>
-        <translation type="unfinished"></translation>
+        <translation>Đặt</translation>
     </message>
     <message>
         <location filename="../displaypositionwidget.cpp" line="144"/>
         <source>No Powered Displays</source>
-        <translation type="unfinished"></translation>
+        <translation>Không có màn hình nào bặt</translation>
     </message>
     <message>
         <location filename="../displaypositionwidget.cpp" line="144"/>
         <source>All available displays in your configuration are off. Are you sure you want to set this screen configuration?</source>
-        <translation type="unfinished"></translation>
+        <translation>Mỗi màn hình trong cấu hình này bị tắt. Bạn có chắc chắn muốn đặt cấu hình màn hình này không?</translation>
     </message>
 </context>
 </TS>
