9484fe6608fbe3f0774838cad2ae16c5ffd9e95f
Victor Tran <vicr12345@gmail.com>
2019-03-30 21:46:36 +1100

    Add translations

---
+++ statuscenter/DisplayPane/translations/fr_FR.ts
@@ -0,0 +1,192 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en_US">
+<context>
+    <name>DisplayArrangementWidget</name>
+    <message>
+        <location filename="../displayarrangementwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displayarrangementwidget.ui" line="28"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DisplayConfigurationWidget</name>
+    <message>
+        <location filename="../displayconfigurationwidget.ui" line="20"/>
+        <source>Frame</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displayconfigurationwidget.ui" line="63"/>
+        <source>Display Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displayconfigurationwidget.ui" line="79"/>
+        <source>Set as Default monitor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displayconfigurationwidget.ui" line="107"/>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displayconfigurationwidget.ui" line="155"/>
+        <source>Resolution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displayconfigurationwidget.ui" line="162"/>
+        <source>Power</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displayconfigurationwidget.ui" line="186"/>
+        <source>Refresh Rate</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DisplayPane</name>
+    <message>
+        <location filename="../displaypane.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="40"/>
+        <location filename="../displaypane.cpp" line="115"/>
+        <source>Display</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="135"/>
+        <source>Screen Scaling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="145"/>
+        <source>If apps and buttons look too small on your device, you can scale them up to improve usability.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="161"/>
+        <source>SCALING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="189"/>
+        <source>&amp;100% (96 dpi)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="196"/>
+        <source>1&amp;50% (144 dpi)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="203"/>
+        <source>&amp;200% (192 dpi)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="210"/>
+        <source>&amp;300% (288 dpi)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="217"/>
+        <source>DPI settings will be applied at next login.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="262"/>
+        <source>If the start time is later than the end time, Redshift will be activated until the end time on the next day.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="294"/>
+        <source>Scheduled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="318"/>
+        <source>End Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="349"/>
+        <source>We&apos;ll send your current location to https://sunrise-sunset.org/ to determine sunlight times if you enable this option.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="377"/>
+        <source>Use Sunlight Cycle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="403"/>
+        <source>Redshift Intensity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="416"/>
+        <source>REDSHIFT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="423"/>
+        <source>Start Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.ui" line="430"/>
+        <source>Redshift tints the display a warmer shade at night, which may help you get a better night&apos;s sleep.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypane.cpp" line="89"/>
+        <source>Redshift</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DisplayPositionWidget</name>
+    <message>
+        <location filename="../displaypositionwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypositionwidget.ui" line="26"/>
+        <source>SCREENS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypositionwidget.ui" line="33"/>
+        <source>Select a screen to configure it, or drag it around to reposition it.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypositionwidget.ui" line="78"/>
+        <source>Set</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypositionwidget.cpp" line="144"/>
+        <source>No Powered Displays</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaypositionwidget.cpp" line="144"/>
+        <source>All available displays in your configuration are off. Are you sure you want to set this screen configuration?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
