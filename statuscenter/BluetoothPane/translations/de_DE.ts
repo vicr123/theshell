053d3b52ea5166c073598755d065670ccb448b8e
Adrian Ulbrich <adrian.ulbrich.2003@gmail.com>
2019-08-03 15:12:23 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (95 of 95 strings)
    
    Translation: theShell/theShell Bluetooth
    Translate-URL: https://translate.vicr123.com/projects/theshell/theshell-bluetooth/de/

--- statuscenter/BluetoothPane/translations/de_DE.ts
+++ statuscenter/BluetoothPane/translations/de_DE.ts
@@ -7,7 +7,7 @@
         <location filename="../btagent.cpp" line="54"/>
         <location filename="../btagent.cpp" line="94"/>
         <source>Key in this code</source>
-        <translation type="unfinished">Gib diesen Code ein</translation>
+        <translation>Gib diesen Code ein</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="77"/>
@@ -148,200 +148,200 @@
         <location filename="../bluetoothmanagement.ui" line="737"/>
         <location filename="../bluetoothmanagement.cpp" line="285"/>
         <source>Unpair Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Geräten Unpairen</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="793"/>
         <source>Device Address</source>
-        <translation type="unfinished"></translation>
+        <translation>Geräteadresse</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="816"/>
         <source>DEVICE INFORMATION</source>
-        <translation type="unfinished"></translation>
+        <translation>GERÄTE-INFORMATIONEN</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="844"/>
         <source>DEVICE ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>GERÄTE-AKTIONEN</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="854"/>
         <location filename="../bluetoothmanagement.cpp" line="238"/>
         <source>Connect to Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Mit Gerät verbinden</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="857"/>
         <source>Establish a connection with this Bluetooth device</source>
-        <translation type="unfinished"></translation>
+        <translation>Eine Verbindung mit diesem Bluetooth-Gerät herstellen</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="880"/>
         <source>Connect to the Internet</source>
-        <translation type="unfinished"></translation>
+        <translation>Mit dem Internet verbinden</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="883"/>
         <source>Establish a connection with this device to access the internet</source>
-        <translation type="unfinished"></translation>
+        <translation>Stelle eine Verbindung mit diesem Gerät her um auf das Internet zuzugreifen</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="890"/>
         <source>Send Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Dateien senden</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="893"/>
         <source>Sends a file to this device</source>
-        <translation type="unfinished"></translation>
+        <translation>Sendet eine Datei an dieses Gerät</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="968"/>
         <location filename="../bluetoothmanagement.ui" line="1188"/>
         <source>Pair Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Gerät Pairen</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="993"/>
         <source>Select a device to pair</source>
-        <translation type="unfinished"></translation>
+        <translation>Ein Gerät zum pairen wählen</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="1254"/>
         <location filename="../bluetoothmanagement.ui" line="1415"/>
         <source>Complete Pairing</source>
-        <translation type="unfinished"></translation>
+        <translation>Pairing abschließen</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="1266"/>
         <source>Check this code matches</source>
-        <translation type="unfinished"></translation>
+        <translation>Überprüfe ob der Code übereinstimmt</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="1273"/>
         <source>Then hit ENTER or RETURN.</source>
-        <translation type="unfinished"></translation>
+        <translation>Drücke dann ENTER.</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="1309"/>
         <source>000000</source>
-        <translation type="unfinished"></translation>
+        <translation>000000</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="1427"/>
         <source>Enter Pairing Code</source>
-        <translation type="unfinished"></translation>
+        <translation>Gib den Pairing-Code ein</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="1557"/>
         <source>No Transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Übertragung</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="1605"/>
         <source>No Transfer Selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Übertragung ausgewählt</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="1615"/>
         <source>Select a transfer for more details</source>
-        <translation type="unfinished"></translation>
+        <translation>Wähle eine Übertragung für mehr Details</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="1737"/>
         <source>Transferred</source>
-        <translation type="unfinished"></translation>
+        <translation>Übertragen</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.ui" line="1744"/>
         <source>File Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Dateigröße</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="188"/>
         <source>Device Name: &lt;b&gt;%1&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Gerätename: &lt;b&gt;%1&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="189"/>
         <source>Address: &lt;b&gt;%1&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Addresse: &lt;b&gt;%1&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="211"/>
         <source>Rename Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Gerät umbenennen</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="211"/>
         <source>What do you want to call this device?</source>
-        <translation type="unfinished"></translation>
+        <translation>Wie möchtest du das Gerät nennen?</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="235"/>
         <source>Disconnect from Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Verbindung mit Gerät trennen</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="236"/>
         <source>Stop communicating with this Bluetooth device</source>
-        <translation type="unfinished"></translation>
+        <translation>Kommunikation mit diesem Bluetoothgerät stoppen</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="239"/>
         <source>Connect to all available Bluetooth profiles on this device</source>
-        <translation type="unfinished"></translation>
+        <translation>Mit allen verfügbaren Bluetooth-Profilen auf diesem Gerät verbinden</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="270"/>
         <source>Bluetooth needs to be enabled to pair a new device. Do you want to enable Bluetooth now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth muss zum Pairen mit Geräten aktiviert sein. Bluetooth jetzt einschalten?</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="285"/>
         <source>Unpair %1 from this device?</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 von diesem Gerät unpairen?</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="289"/>
         <source>Unpair</source>
-        <translation type="unfinished"></translation>
+        <translation>Unpairen</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="290"/>
         <source>%1 has been unpaired from this device.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 wurde unpaired.</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="315"/>
         <source>Pairing Complete</source>
-        <translation type="unfinished"></translation>
+        <translation>Pairing abgeschlossen</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="316"/>
         <source>%1 has been paired with this device.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 wurde mit diesem Gerät gepaired.</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="369"/>
         <source>Pairing Failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Pairing gescheitert</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="370"/>
         <source>Couldn&apos;t pair with %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Konnte nicht mit %1 pairen.</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="438"/>
         <source>Send Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Error senden</translation>
     </message>
     <message>
         <location filename="../bluetoothmanagement.cpp" line="439"/>
         <source>Couldn&apos;t send the file to %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Konnte die Datei nicht an %1 senden.</translation>
     </message>
 </context>
 <context>
@@ -349,88 +349,86 @@
     <message numerus="yes">
         <location filename="../chunkwidget.cpp" line="54"/>
         <source>%n devices</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n Gerät</numerusform>
+        <numerusform>%n Geräte</numerusform>
+        </translation></message>
 </context>
 <context>
     <name>DevicesListModel</name>
     <message>
         <location filename="../deviceslistmodel.cpp" line="96"/>
         <source>Mobile Phone</source>
-        <translation type="unfinished"></translation>
+        <translation>Mobiltelefon</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="98"/>
         <source>Computer</source>
-        <translation type="unfinished"></translation>
+        <translation>Rechner</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="100"/>
         <source>AV</source>
-        <translation type="unfinished"></translation>
+        <translation>AV</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="103"/>
         <source>Speaker</source>
-        <translation type="unfinished"></translation>
+        <translation>Lautsprecher</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="105"/>
         <source>Camera</source>
-        <translation type="unfinished"></translation>
+        <translation>Kamera</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="107"/>
         <source>Health</source>
-        <translation type="unfinished"></translation>
+        <translation>Gesundheit</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="109"/>
         <source>Imaging</source>
-        <translation type="unfinished"></translation>
+        <translation>Imaging</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="111"/>
         <source>Keyboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Tastatur</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="113"/>
         <source>Mouse</source>
-        <translation type="unfinished"></translation>
+        <translation>Maus</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="115"/>
         <source>Joypad</source>
-        <translation type="unfinished"></translation>
+        <translation>Joypad</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="117"/>
         <source>Peripheral</source>
-        <translation type="unfinished"></translation>
+        <translation>Peripherie</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="119"/>
         <source>Printer</source>
-        <translation type="unfinished"></translation>
+        <translation>Drucker</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="121"/>
         <source>Network</source>
-        <translation type="unfinished"></translation>
+        <translation>Netzwerk</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="123"/>
         <source>Modem</source>
-        <translation type="unfinished"></translation>
+        <translation>Modem</translation>
     </message>
     <message>
         <location filename="../deviceslistmodel.cpp" line="125"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekannt</translation>
     </message>
 </context>
 <context>
@@ -438,27 +436,27 @@
     <message>
         <location filename="../transferslistmodel.cpp" line="175"/>
         <source>Failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Gescheitert</translation>
     </message>
     <message>
         <location filename="../transferslistmodel.cpp" line="179"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Fertig</translation>
     </message>
     <message>
         <location filename="../transferslistmodel.cpp" line="183"/>
         <source>Queued</source>
-        <translation type="unfinished"></translation>
+        <translation>Eingereiht</translation>
     </message>
     <message>
         <location filename="../transferslistmodel.cpp" line="187"/>
         <source>Paused</source>
-        <translation type="unfinished"></translation>
+        <translation>Pausiert</translation>
     </message>
     <message>
         <location filename="../transferslistmodel.cpp" line="191"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekannt</translation>
     </message>
 </context>
 <context>
@@ -466,52 +464,52 @@
     <message>
         <location filename="../transferslistmodel.cpp" line="70"/>
         <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth</translation>
     </message>
     <message>
         <location filename="../transferslistmodel.cpp" line="81"/>
         <source>Receiving File over Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Dateien über Bluetooth empfangen</translation>
     </message>
     <message>
         <location filename="../transferslistmodel.cpp" line="81"/>
         <source>Sending File over Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Sende Datei über Bluetooth</translation>
     </message>
     <message>
         <location filename="../transferslistmodel.cpp" line="82"/>
         <source>File Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Dateiname</translation>
     </message>
     <message>
         <location filename="../transferslistmodel.cpp" line="116"/>
         <source>Transfer Complete</source>
-        <translation type="unfinished"></translation>
+        <translation>Übertragung abgeschlossen</translation>
     </message>
     <message>
         <location filename="../transferslistmodel.cpp" line="118"/>
         <source>&lt;b&gt;%1&lt;/b&gt; has been successfully received and placed inside Downloads &gt; Bluetooth.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; wurde erfolgreich übertragen und in Downloads &gt; Bluetooth platziert.</translation>
     </message>
     <message>
         <location filename="../transferslistmodel.cpp" line="120"/>
         <source>&lt;b&gt;%1&lt;/b&gt; has been sent successfully.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; wurde erfolgreich gesendet.</translation>
     </message>
     <message>
         <location filename="../transferslistmodel.cpp" line="127"/>
         <source>Transfer Failure</source>
-        <translation type="unfinished"></translation>
+        <translation>Übertragung fehlgeschlagen</translation>
     </message>
     <message>
         <location filename="../transferslistmodel.cpp" line="129"/>
         <source>There was a problem while receiving &lt;b&gt;%1&lt;/b&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Beim Übertragen von&lt;b&gt;%1&lt;/b&gt; ist ein Problem aufgetreten.</translation>
     </message>
     <message>
         <location filename="../transferslistmodel.cpp" line="131"/>
         <source>There was a problem while sending &lt;b&gt;%1&lt;/b&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Beim empfangen von &lt;b&gt;%1&lt;/b&gt; ist ein Problem aufgetreten.</translation>
     </message>
 </context>
 </TS>
