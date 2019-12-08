4cde8a31424d962df5ec0755e2f423e057f99c71
Victor Tran <vicr12345@gmail.com>
2019-12-09 03:59:15 +1100

    Add missing translations

---
+++ statuscenter/PowerPane/translations/sv_SE.ts
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="se_NO" sourcelanguage="en_US">
+<context>
+    <name>PowerDevice</name>
+    <message>
+        <location filename="../powerdevice.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powerdevice.ui" line="79"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powerdevice.cpp" line="58"/>
+        <source>Charging</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powerdevice.cpp" line="69"/>
+        <source>Full</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powerdevice.cpp" line="80"/>
+        <source>Discharging</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powerdevice.cpp" line="108"/>
+        <source>Connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powerdevice.cpp" line="110"/>
+        <source>Disconnected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powerdevice.cpp" line="127"/>
+        <source>Power Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PowerPane</name>
+    <message>
+        <location filename="../powerpane.ui" line="40"/>
+        <location filename="../powerpane.cpp" line="97"/>
+        <source>Power</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powerpane.ui" line="105"/>
+        <source>SCREEN BRIGHTNESS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powerpane.ui" line="168"/>
+        <source>Power Stretch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powerpane.ui" line="181"/>
+        <source>POWER STRETCH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powerpane.ui" line="188"/>
+        <source>Running low on power? Turn on Power Stretch to stretch out your battery to last as long as possible.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powerpane.ui" line="236"/>
+        <source>DEVICES</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
