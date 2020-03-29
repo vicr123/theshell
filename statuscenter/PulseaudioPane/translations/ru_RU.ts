42370e89aeb6ceac1b1e4225a0cf10f5df11ea56
Антон Климанов <klimanov625@outlook.com>
2020-03-29 21:08:13 +0000

    Translated using Weblate (Russian)
    Currently translated at 97.0% (66 of 68 strings)
    
    Translation: theShell/theShell Audio
    Translate-URL: https://translate.vicr123.com/projects/theshell/theshell-audio/ru/

--- statuscenter/PulseaudioPane/translations/ru_RU.ts
+++ statuscenter/PulseaudioPane/translations/ru_RU.ts
@@ -6,13 +6,13 @@
     <message>
         <location filename="../audiopane.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Композиция</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="95"/>
         <location filename="../audiopane.cpp" line="169"/>
         <source>Audio</source>
-        <translation type="unfinished"></translation>
+        <translation>Аудио</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="124"/>
@@ -24,61 +24,61 @@
         <location filename="../audiopane.ui" line="129"/>
         <location filename="../audiopane.ui" line="435"/>
         <source>Playback</source>
-        <translation type="unfinished"></translation>
+        <translation>Воспроизведение</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="134"/>
         <location filename="../audiopane.ui" line="542"/>
         <source>Recording</source>
-        <translation type="unfinished"></translation>
+        <translation>Запись</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="139"/>
         <location filename="../audiopane.ui" line="649"/>
         <source>Output Devices</source>
-        <translation type="unfinished"></translation>
+        <translation>Устройства вывода</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="144"/>
         <location filename="../audiopane.ui" line="856"/>
         <source>Input Devices</source>
-        <translation type="unfinished"></translation>
+        <translation>Устройства ввода</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="149"/>
         <location filename="../audiopane.ui" line="963"/>
         <source>Advanced Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Расширенная настройка</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="273"/>
         <source>BEHAVIOUR</source>
-        <translation type="unfinished"></translation>
+        <translation>ПОВЕДЕНИЕ</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="293"/>
         <source>Volume Overdrive</source>
-        <translation type="unfinished"></translation>
+        <translation>Расширенная громкость</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="300"/>
         <source>Volume Overdrive allows you to set your volume higher than 100%</source>
-        <translation type="unfinished"></translation>
+        <translation>Расширенная громкость позволяет установить громкость выше 100%</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="365"/>
         <source>SOUNDS</source>
-        <translation type="unfinished"></translation>
+        <translation>ЗВУКИ</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="746"/>
         <source>QUIET MODE</source>
-        <translation type="unfinished"></translation>
+        <translation>ТИХИЙ РЕЖИМ</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="778"/>
         <source>Turn off Quiet Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Выключить тихий режим</translation>
     </message>
     <message>
         <location filename="../audiopane.cpp" line="110"/>
@@ -88,57 +88,57 @@
     <message>
         <location filename="../audiopane.cpp" line="112"/>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Информация</translation>
     </message>
     <message>
         <location filename="../audiopane.cpp" line="113"/>
         <source>Question</source>
-        <translation type="unfinished"></translation>
+        <translation>Вопрос</translation>
     </message>
     <message>
         <location filename="../audiopane.cpp" line="114"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Внимание</translation>
     </message>
     <message>
         <location filename="../audiopane.cpp" line="115"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка</translation>
     </message>
     <message>
         <location filename="../audiopane.cpp" line="125"/>
         <source>Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Громкость</translation>
     </message>
     <message>
         <location filename="../audiopane.cpp" line="125"/>
         <source>Quiet Mode is set to Mute.</source>
-        <translation type="unfinished"></translation>
+        <translation>Тихий режим установлен на заглушение.</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="349"/>
         <source>You can change notification sounds in the Notifications pane</source>
-        <translation type="unfinished"></translation>
+        <translation>Вы можете изменить звук уведомлений в панели уведомлений</translation>
     </message>
     <message>
         <location filename="../audiopane.cpp" line="117"/>
         <source>Volume Change</source>
-        <translation type="unfinished"></translation>
+        <translation>Перемена громкости</translation>
     </message>
     <message>
         <location filename="../audiopane.cpp" line="111"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>Выход</translation>
     </message>
     <message>
         <location filename="../audiopane.ui" line="342"/>
         <source>Sound Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Тема звука</translation>
     </message>
     <message>
         <location filename="../audiopane.cpp" line="116"/>
         <source>Screenshot</source>
-        <translation type="unfinished"></translation>
+        <translation>Снимок экрана</translation>
     </message>
 </context>
 <context>
@@ -146,12 +146,12 @@
     <message>
         <location filename="../cardwidget.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Форма</translation>
     </message>
     <message>
         <location filename="../cardwidget.ui" line="52"/>
         <source>Profile</source>
-        <translation type="unfinished"></translation>
+        <translation>Профиль</translation>
     </message>
 </context>
 <context>
@@ -159,29 +159,29 @@
     <message>
         <location filename="../deviceselection.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Форма</translation>
     </message>
     <message>
         <location filename="../deviceselection.ui" line="71"/>
         <location filename="../deviceselection.cpp" line="36"/>
         <source>Select Output Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите устройство вывода</translation>
     </message>
     <message>
         <location filename="../deviceselection.ui" line="96"/>
         <location filename="../deviceselection.cpp" line="37"/>
         <source>Which device do you want this sound source to be played on?</source>
-        <translation type="unfinished"></translation>
+        <translation>На каком устройстве будет выводиться звук?</translation>
     </message>
     <message>
         <location filename="../deviceselection.cpp" line="43"/>
         <source>Select Input Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите устройство ввода</translation>
     </message>
     <message>
         <location filename="../deviceselection.cpp" line="44"/>
         <source>Which device do you want this app to listen to?</source>
-        <translation type="unfinished"></translation>
+        <translation>Какое устройство будет слышать это приложение?</translation>
     </message>
 </context>
 <context>
@@ -189,12 +189,12 @@
     <message>
         <location filename="../sinkinputwidget.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Форма</translation>
     </message>
     <message>
         <location filename="../sinkinputwidget.cpp" line="105"/>
         <source>Playback</source>
-        <translation type="unfinished"></translation>
+        <translation>Проигрывание</translation>
     </message>
 </context>
 <context>
@@ -202,42 +202,42 @@
     <message>
         <location filename="../sinkwidget.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Форма</translation>
     </message>
     <message>
         <location filename="../sinkwidget.ui" line="99"/>
         <source>Set Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Установить по умолчанию</translation>
     </message>
     <message>
         <location filename="../sinkwidget.ui" line="125"/>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>Заглушить</translation>
     </message>
     <message>
         <location filename="../sinkwidget.ui" line="181"/>
         <source>Master</source>
-        <translation type="unfinished"></translation>
+        <translation>Главный</translation>
     </message>
     <message>
         <location filename="../sinkwidget.cpp" line="120"/>
         <source>Speaker</source>
-        <translation type="unfinished"></translation>
+        <translation>Громкоговоритель</translation>
     </message>
     <message>
         <location filename="../sinkwidget.cpp" line="254"/>
         <source>Speakers</source>
-        <translation type="unfinished"></translation>
+        <translation>Громкоговорители</translation>
     </message>
     <message>
         <location filename="../sinkwidget.cpp" line="257"/>
         <source>Headphones</source>
-        <translation type="unfinished"></translation>
+        <translation>Наушники</translation>
     </message>
     <message>
         <location filename="../sinkwidget.cpp" line="260"/>
         <source>Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Громкость</translation>
     </message>
 </context>
 <context>
@@ -245,12 +245,12 @@
     <message>
         <location filename="../sourceoutputwidget.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Форма</translation>
     </message>
     <message>
         <location filename="../sourceoutputwidget.cpp" line="106"/>
         <source>Recording</source>
-        <translation type="unfinished"></translation>
+        <translation>Запись</translation>
     </message>
 </context>
 <context>
@@ -258,17 +258,17 @@
     <message>
         <location filename="../sourcewidget.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Форма</translation>
     </message>
     <message>
         <location filename="../sourcewidget.ui" line="90"/>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>Заглушить</translation>
     </message>
     <message>
         <location filename="../sourcewidget.ui" line="129"/>
         <source>Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Громкость</translation>
     </message>
 </context>
 <context>
@@ -276,102 +276,102 @@
     <message>
         <location filename="../maps.h" line="27"/>
         <source>Invalid</source>
-        <translation type="unfinished"></translation>
+        <translation>Неверно</translation>
     </message>
     <message>
         <location filename="../maps.h" line="28"/>
         <source>Mono</source>
-        <translation type="unfinished"></translation>
+        <translation>Моно</translation>
     </message>
     <message>
         <location filename="../maps.h" line="29"/>
         <source>Front Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Фронтальный левый</translation>
     </message>
     <message>
         <location filename="../maps.h" line="30"/>
         <source>Front Center</source>
-        <translation type="unfinished"></translation>
+        <translation>Фронтальный центр</translation>
     </message>
     <message>
         <location filename="../maps.h" line="31"/>
         <source>Front Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Фронтальный правый</translation>
     </message>
     <message>
         <location filename="../maps.h" line="32"/>
         <source>Rear Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Задний левый</translation>
     </message>
     <message>
         <location filename="../maps.h" line="33"/>
         <source>Rear Center</source>
-        <translation type="unfinished"></translation>
+        <translation>Задний центр</translation>
     </message>
     <message>
         <location filename="../maps.h" line="34"/>
         <source>Rear Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Задний правый</translation>
     </message>
     <message>
         <location filename="../maps.h" line="35"/>
         <source>Subwoofer</source>
-        <translation type="unfinished"></translation>
+        <translation>Сабвуфер</translation>
     </message>
     <message>
         <location filename="../maps.h" line="36"/>
         <source>Forward Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Передний левый</translation>
     </message>
     <message>
         <location filename="../maps.h" line="37"/>
         <source>Forward Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Передний правый</translation>
     </message>
     <message>
         <location filename="../maps.h" line="38"/>
         <source>Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Левый</translation>
     </message>
     <message>
         <location filename="../maps.h" line="39"/>
         <source>Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Правый</translation>
     </message>
     <message>
         <location filename="../maps.h" line="40"/>
         <source>Overhead</source>
-        <translation type="unfinished"></translation>
+        <translation>Верх</translation>
     </message>
     <message>
         <location filename="../maps.h" line="41"/>
         <source>Overhead Front Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Верх фронтальный левый</translation>
     </message>
     <message>
         <location filename="../maps.h" line="42"/>
         <source>Overhead Front Center</source>
-        <translation type="unfinished"></translation>
+        <translation>Верх фронтальный центр</translation>
     </message>
     <message>
         <location filename="../maps.h" line="43"/>
         <source>Overhead Front Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Верх фронтальный правый</translation>
     </message>
     <message>
         <location filename="../maps.h" line="44"/>
         <source>Overhead Rear Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Верх задний левый</translation>
     </message>
     <message>
         <location filename="../maps.h" line="45"/>
         <source>Overhead Rear Center</source>
-        <translation type="unfinished"></translation>
+        <translation>Верх задний центр</translation>
     </message>
     <message>
         <location filename="../maps.h" line="46"/>
         <source>Overhead Rear Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Верх задний правый</translation>
     </message>
 </context>
 </TS>
