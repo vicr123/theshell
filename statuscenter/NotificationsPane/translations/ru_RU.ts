facb98170df2039ef850938860f453fdd75810be
Антон Климанов <klimanov625@outlook.com>
2020-03-30 12:19:30 +0000

    Translated using Weblate (Russian)
    Currently translated at 96.4% (54 of 56 strings)
    
    Translation: theShell/theShell Notifications
    Translate-URL: https://translate.vicr123.com/projects/theshell/theshell-notifications/ru/

--- statuscenter/NotificationsPane/translations/ru_RU.ts
+++ statuscenter/NotificationsPane/translations/ru_RU.ts
@@ -12,10 +12,10 @@
     <name>JobServer</name>
     <message numerus="yes">
         <source>%n jobs running</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n задача исполняется</numerusform>
+            <numerusform>%n задачи исполняются</numerusform>
+            <numerusform>%n задач исполняется</numerusform>
         </translation>
     </message>
 </context>
@@ -23,21 +23,21 @@
     <name>JobViewWidget</name>
     <message>
         <source>Frame</source>
-        <translation type="unfinished"></translation>
+        <translation>Рамка</translation>
     </message>
 </context>
 <context>
     <name>NotificationAppGroup</name>
     <message>
         <source>Collapse Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Свернуть уведомления</translation>
     </message>
     <message numerus="yes">
         <source>+%n notifications collapsed</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>+%n задача свёрнута</numerusform>
+            <numerusform>+%n задачи свёрнуто</numerusform>
+            <numerusform>+%n задач свёрнуто</numerusform>
         </translation>
     </message>
 </context>
@@ -45,97 +45,97 @@
     <name>NotificationPanel</name>
     <message numerus="yes">
         <source>%n d</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n д</numerusform>
+            <numerusform>%n д</numerusform>
+            <numerusform>%n д</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>%n hr</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n ч</numerusform>
+            <numerusform>%n ч</numerusform>
+            <numerusform>%n ч</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <source>%n min</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n м</numerusform>
+            <numerusform>%n м</numerusform>
+            <numerusform>%n м</numerusform>
         </translation>
     </message>
     <message>
         <source>just now</source>
-        <translation type="unfinished"></translation>
+        <translation>только что</translation>
     </message>
 </context>
 <context>
     <name>NotificationPopup</name>
     <message>
         <source>Dismiss</source>
-        <translation type="unfinished"></translation>
+        <translation>Отклонить</translation>
     </message>
     <message>
         <source>Hide</source>
-        <translation type="unfinished"></translation>
+        <translation>Спрятать</translation>
     </message>
 </context>
 <context>
     <name>NotificationsWidget</name>
     <message>
         <source>No notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Нет уведомлений</translation>
     </message>
     <message>
         <source>There&apos;s nothing to see at the moment. Don&apos;t worry, we&apos;ll be sure to tell you as soon as something pops up.</source>
-        <translation type="unfinished"></translation>
+        <translation>Пока что тут пусто. Не беспокойтесь, мы сообщим вам как только что-то произойдёт.</translation>
     </message>
     <message>
         <source>Clear All</source>
-        <translation type="unfinished"></translation>
+        <translation>Очистить все</translation>
     </message>
     <message>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Уведомления</translation>
     </message>
     <message>
         <source>Quiet Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Тихий режим</translation>
     </message>
     <message>
         <source>Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>Звук</translation>
     </message>
     <message>
         <source>Critical Only</source>
-        <translation type="unfinished"></translation>
+        <translation>Только важные</translation>
     </message>
     <message>
         <source>No Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Нет уведомлений</translation>
     </message>
     <message>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>Заглушить</translation>
     </message>
     <message>
         <source>Fore&amp;ver</source>
-        <translation type="unfinished"></translation>
+        <translation>Навсе&amp;гда</translation>
     </message>
     <message>
         <source>Turn off in</source>
-        <translation type="unfinished"></translation>
+        <translation>Выключить через</translation>
     </message>
     <message>
         <source>HH:mm</source>
-        <translation type="unfinished"></translation>
+        <translation>ЧЧ:мм</translation>
     </message>
     <message>
         <source>Turn off at</source>
-        <translation type="unfinished"></translation>
+        <translation>Выключить в</translation>
     </message>
     <message numerus="yes">
         <source>%n notification(s)</source>
@@ -150,11 +150,11 @@
     <name>SettingsPane</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Форма</translation>
     </message>
     <message>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Уведомления</translation>
     </message>
     <message>
         <source>General</source>
@@ -162,117 +162,119 @@
     </message>
     <message>
         <source>Show Contents</source>
-        <translation type="unfinished"></translation>
+        <translation>Показать содержание</translation>
     </message>
     <message>
         <source>Hide Contents</source>
-        <translation type="unfinished"></translation>
+        <translation>Спрятать содержание</translation>
     </message>
     <message>
         <source>Don&apos;t Show</source>
-        <translation type="unfinished"></translation>
+        <translation>Не показывать</translation>
     </message>
     <message>
         <source>On lock screen</source>
-        <translation type="unfinished"></translation>
+        <translation>На экране блокировки</translation>
     </message>
     <message>
         <source>DISPLAY</source>
-        <translation type="unfinished"></translation>
+        <translation>ДИСПЛЕЙ</translation>
     </message>
     <message>
         <source>Emphasise sending app</source>
-        <translation type="unfinished"></translation>
+        <translation>Приоритизировать отправляющее приложение</translation>
     </message>
     <message>
         <source>Attenuate other sounds on notification</source>
-        <translation type="unfinished"></translation>
+        <translation>Приглушать звуки при уведомлении</translation>
     </message>
     <message>
         <source>Triple Ping</source>
-        <translation type="unfinished"></translation>
+        <translation>Тройной звонок</translation>
     </message>
     <message>
         <source>Upside Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Перевёрнуто</translation>
     </message>
     <message>
         <source>Echo</source>
-        <translation type="unfinished"></translation>
+        <translation>Эхо</translation>
     </message>
     <message>
         <source>Notification Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>Звук уведомления</translation>
     </message>
     <message>
         <source>SOUNDS</source>
-        <translation type="unfinished"></translation>
+        <translation>ЗВУКИ</translation>
     </message>
     <message>
         <source>Allow Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Разрешить уведомления</translation>
     </message>
     <message>
         <source>BEHAVIOUR</source>
-        <translation type="unfinished"></translation>
+        <translation>ПОВЕДЕНИЕ</translation>
     </message>
     <message>
         <source>Pop up onscreen</source>
-        <translation type="unfinished"></translation>
+        <translation>Показывать на экране</translation>
     </message>
     <message>
         <source>Play Sounds</source>
-        <translation type="unfinished"></translation>
+        <translation>Проигрывать звуки</translation>
     </message>
     <message>
         <source>Bypass Quiet Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Игнорировать Тихий режим</translation>
     </message>
     <message>
         <source>Even when Quiet Mode is set to No Notifications or Critical Only, notifications from this app will continue to show</source>
-        <translation type="unfinished"></translation>
+        <translation>Даже когда Тихий режим установлен на Без уведомлений или Только важные, уведомления от этого приложения всё равно продолжат показываться</translation>
     </message>
     <message>
         <source>Media Connection</source>
-        <translation type="unfinished"></translation>
+        <translation>Медиа соединение</translation>
     </message>
     <message>
         <source>SYSTEM NOTIFICATIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>СИСТЕМНЫЕ УВЕДОМЛЕНИЯ</translation>
     </message>
     <message>
         <source>Charging</source>
-        <translation type="unfinished"></translation>
+        <translation>Зарядка</translation>
     </message>
     <message>
         <source>Unplugged</source>
-        <translation type="unfinished"></translation>
+        <translation>Отсоединено</translation>
     </message>
     <message>
         <source>Notification Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Громкость уведомлений</translation>
     </message>
     <message>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>ДЕЙСТВИЯ</translation>
     </message>
     <message>
         <source>Mark as uninstalled</source>
-        <translation type="unfinished"></translation>
+        <translation>Отметить как удалённое</translation>
     </message>
     <message>
         <source>If this application has been uninstalled, you can remove this application from the list of notifications.</source>
-        <translation type="unfinished"></translation>
+        <translation>Если это приложение было удалено, вы можете удалить это приложение из списка уведомлений.</translation>
     </message>
     <message>
         <source>Mark as uninstalled?</source>
-        <translation type="unfinished"></translation>
+        <translation>Отметить как удалённое?</translation>
     </message>
     <message>
         <source>This will remove the settings from theShell. If the application sends another notification, it will reappear.
 
 Mark &quot;%1&quot; as uninstalled?</source>
-        <translation type="unfinished"></translation>
+        <translation>Это удалит настройки из theShell. Если приложение отправит уведомление, оно снова появится.
+
+Отметить &quot;%1&quot; как удалённое?</translation>
     </message>
 </context>
 </TS>
