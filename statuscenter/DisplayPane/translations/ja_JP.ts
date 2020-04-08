d3e044ca5865c9b382c919ff03a5eba2bf29ae08
Hayao0819 <shun819.mail@gmail.com>
2020-04-08 06:45:42 +0000

    Translated using Weblate (Japanese)
    Currently translated at 96.7% (30 of 31 strings)
    
    Translation: theShell/theShell Display
    Translate-URL: https://translate.vicr123.com/projects/theshell/theshell-display/ja/

--- statuscenter/DisplayPane/translations/ja_JP.ts
+++ statuscenter/DisplayPane/translations/ja_JP.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="ja_JP">
 <context>
     <name>DisplayArrangementWidget</name>
     <message>
         <location filename="../displayarrangementwidget.cpp" line="103"/>
         <source>Screen %1 connected</source>
-        <translation type="unfinished"></translation>
+        <translation>画面 %1 が接続されました</translation>
     </message>
     <message>
         <location filename="../displayarrangementwidget.cpp" line="104"/>
         <source>To start using it, configure your screens.</source>
-        <translation type="unfinished"></translation>
+        <translation>使い始めるには画面を設定して下さい。</translation>
     </message>
     <message>
         <location filename="../displayarrangementwidget.cpp" line="105"/>
         <source>Configure Screens</source>
-        <translation type="unfinished"></translation>
+        <translation>画面設定</translation>
     </message>
 </context>
 <context>
@@ -24,27 +24,27 @@
     <message>
         <location filename="../displayconfigurationwidget.ui" line="79"/>
         <source>Set as Default monitor</source>
-        <translation type="unfinished"></translation>
+        <translation>デフォルトとして設定する</translation>
     </message>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="108"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>完了しました</translation>
     </message>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="157"/>
         <source>Resolution</source>
-        <translation type="unfinished"></translation>
+        <translation>解決</translation>
     </message>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="164"/>
         <source>Power</source>
-        <translation type="unfinished"></translation>
+        <translation>電源</translation>
     </message>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="188"/>
         <source>Refresh Rate</source>
-        <translation type="unfinished"></translation>
+        <translation>リフレッシュレート</translation>
     </message>
 </context>
 <context>
@@ -53,92 +53,92 @@
         <location filename="../displaypane.ui" line="40"/>
         <location filename="../displaypane.cpp" line="124"/>
         <source>Display</source>
-        <translation type="unfinished"></translation>
+        <translation>ディスプレイ</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="135"/>
         <source>Screen Scaling</source>
-        <translation type="unfinished"></translation>
+        <translation>スクリーンスケーリング</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="145"/>
         <source>If apps and buttons look too small on your device, you can scale them up to improve usability.</source>
-        <translation type="unfinished"></translation>
+        <translation>アプリやボタンが小さすぎる場合は、拡大して調整できます。</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="161"/>
         <source>SCALING</source>
-        <translation type="unfinished"></translation>
+        <translation>スケーリング</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="189"/>
         <source>&amp;100% (96 dpi)</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;100% (96 dpi)</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="196"/>
         <source>1&amp;50% (144 dpi)</source>
-        <translation type="unfinished"></translation>
+        <translation>1&amp;50% (144 dpi)</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="203"/>
         <source>&amp;200% (192 dpi)</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;200% (192 dpi)</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="210"/>
         <source>&amp;300% (288 dpi)</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;300% (288 dpi)</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="252"/>
         <source>If the start time is later than the end time, Redshift will be activated until the end time on the next day.</source>
-        <translation type="unfinished"></translation>
+        <translation>開始時刻が終了時刻より遅い場合、Redshiftは翌日の終了時刻までアクティブになります。</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="284"/>
         <source>Scheduled</source>
-        <translation type="unfinished"></translation>
+        <translation>予定</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="308"/>
         <source>End Time</source>
-        <translation type="unfinished"></translation>
+        <translation>終了時刻</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="339"/>
         <source>We&apos;ll send your current location to https://sunrise-sunset.org/ to determine sunlight times if you enable this option.</source>
-        <translation type="unfinished"></translation>
+        <translation>このオプションを有効にすると、現在の場所がhttps://sunrise-sunset.org/に送信され、時間が決定されます。</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="367"/>
         <source>Use Sunlight Cycle</source>
-        <translation type="unfinished"></translation>
+        <translation>日光サイクルを使用</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="393"/>
         <source>Redshift Intensity</source>
-        <translation type="unfinished"></translation>
+        <translation>Redshiftの強度</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="406"/>
         <source>REDSHIFT</source>
-        <translation type="unfinished"></translation>
+        <translation>REDSHIFT</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="413"/>
         <source>Start Time</source>
-        <translation type="unfinished"></translation>
+        <translation>開始時刻</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="420"/>
         <source>Redshift blocks blue light so that you can sleep better at night.</source>
-        <translation type="unfinished"></translation>
+        <translation>Redshiftはブルーライトを遮るので、夜はよく眠れます。</translation>
     </message>
     <message>
         <location filename="../displaypane.cpp" line="98"/>
         <source>Redshift</source>
-        <translation type="unfinished"></translation>
+        <translation>Redshift</translation>
     </message>
 </context>
 <context>
@@ -146,17 +146,17 @@
     <message>
         <location filename="../displaypositionwidget.ui" line="26"/>
         <source>SCREENS</source>
-        <translation type="unfinished"></translation>
+        <translation>画面</translation>
     </message>
     <message>
         <location filename="../displaypositionwidget.ui" line="33"/>
         <source>Select a screen to configure it, or drag it around to reposition it. To duplicate displays, drag them on top of each other and set them to have the same resolution.</source>
-        <translation type="unfinished"></translation>
+        <translation>画面を選択して設定するか、ドラッグして位置を変更します。ミラーリングするには、互いの上にドラッグし、同じ解像度を持つように設定します。</translation>
     </message>
     <message>
         <location filename="../displaypositionwidget.ui" line="78"/>
         <source>Set</source>
-        <translation type="unfinished"></translation>
+        <translation>設定</translation>
     </message>
     <message>
         <location filename="../displaypositionwidget.cpp" line="164"/>
@@ -166,7 +166,7 @@
     <message>
         <location filename="../displaypositionwidget.cpp" line="164"/>
         <source>All available displays in your configuration are off. Are you sure you want to set this screen configuration?</source>
-        <translation type="unfinished"></translation>
+        <translation>使用可能なすべてのディスプレイがオフになっています。この設定を保存してよろしいですか？</translation>
     </message>
 </context>
 </TS>
