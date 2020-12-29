f55606feb5d8978d78696112ac90a9c270bef3be
Aleyna Tilki <atahanreis@icloud.com>
2020-12-29 00:31:40 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (31 of 31 strings)
    
    Translation: theShell/theShell Display
    Translate-URL: https://translate.vicr123.com/projects/theshell/theshell-display/tr/

--- statuscenter/DisplayPane/translations/tr_TR.ts
+++ statuscenter/DisplayPane/translations/tr_TR.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../displayarrangementwidget.cpp" line="103"/>
         <source>Screen %1 connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekran %1 bağlandı</translation>
     </message>
     <message>
         <location filename="../displayarrangementwidget.cpp" line="104"/>
         <source>To start using it, configure your screens.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanmaya başlamak için ekranlarınızı yapılandırın.</translation>
     </message>
     <message>
         <location filename="../displayarrangementwidget.cpp" line="105"/>
         <source>Configure Screens</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekranları Yapılandır</translation>
     </message>
 </context>
 <context>
@@ -24,27 +24,27 @@
     <message>
         <location filename="../displayconfigurationwidget.ui" line="79"/>
         <source>Set as Default monitor</source>
-        <translation type="unfinished"></translation>
+        <translation>Varsayılan monitör olarak ayarla</translation>
     </message>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="108"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Bitti</translation>
     </message>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="157"/>
         <source>Resolution</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekran çözünürlüğü</translation>
     </message>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="164"/>
         <source>Power</source>
-        <translation type="unfinished"></translation>
+        <translation>Güç</translation>
     </message>
     <message>
         <location filename="../displayconfigurationwidget.ui" line="188"/>
         <source>Refresh Rate</source>
-        <translation type="unfinished"></translation>
+        <translation>Yenileme hızı</translation>
     </message>
 </context>
 <context>
@@ -53,92 +53,92 @@
         <location filename="../displaypane.ui" line="40"/>
         <location filename="../displaypane.cpp" line="124"/>
         <source>Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekran</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="135"/>
         <source>Screen Scaling</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekran Ölçeklendirme</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="145"/>
         <source>If apps and buttons look too small on your device, you can scale them up to improve usability.</source>
-        <translation type="unfinished"></translation>
+        <translation>Uygulamalar ve düğmeler cihazınızda çok küçük görünüyorsa, kullanılabilirliği iyileştirmek için bunları ölçeklendirebilirsiniz.</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="161"/>
         <source>SCALING</source>
-        <translation type="unfinished"></translation>
+        <translation>ÖLÇEKLENDİRME</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="189"/>
         <source>&amp;100% (96 dpi)</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;100% (96 dpi)</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="196"/>
         <source>1&amp;50% (144 dpi)</source>
-        <translation type="unfinished"></translation>
+        <translation>1&amp;50% (144 dpi)</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="203"/>
         <source>&amp;200% (192 dpi)</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;200% (192 dpi)</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="210"/>
         <source>&amp;300% (288 dpi)</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;300% (288 dpi)</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="252"/>
         <source>If the start time is later than the end time, Redshift will be activated until the end time on the next day.</source>
-        <translation type="unfinished"></translation>
+        <translation>If the start time has passed the end time, Redshift will be activated until the end time the next day.</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="284"/>
         <source>Scheduled</source>
-        <translation type="unfinished"></translation>
+        <translation>Planlandı</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="308"/>
         <source>End Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Bitiş zamanı</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="339"/>
         <source>We&apos;ll send your current location to https://sunrise-sunset.org/ to determine sunlight times if you enable this option.</source>
-        <translation type="unfinished"></translation>
+        <translation>Mevcut konumunuzu şu adrese göndereceğiz: https://sunrise-sunset.org/ bu seçeneği etkinleştirirseniz güneş ışığı sürelerini belirlemek için.</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="367"/>
         <source>Use Sunlight Cycle</source>
-        <translation type="unfinished"></translation>
+        <translation>Güneş Işığı Döngüsünü Kullanın</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="393"/>
         <source>Redshift Intensity</source>
-        <translation type="unfinished"></translation>
+        <translation>Redshift Yoğunluğu</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="406"/>
         <source>REDSHIFT</source>
-        <translation type="unfinished"></translation>
+        <translation>REDSHIFT</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="413"/>
         <source>Start Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Başlangıç saati</translation>
     </message>
     <message>
         <location filename="../displaypane.ui" line="420"/>
         <source>Redshift blocks blue light so that you can sleep better at night.</source>
-        <translation type="unfinished"></translation>
+        <translation>Redshift, gece daha iyi uyuyabilmeniz için mavi ışığı engeller.</translation>
     </message>
     <message>
         <location filename="../displaypane.cpp" line="98"/>
         <source>Redshift</source>
-        <translation type="unfinished"></translation>
+        <translation>Redshift</translation>
     </message>
 </context>
 <context>
@@ -146,27 +146,27 @@
     <message>
         <location filename="../displaypositionwidget.ui" line="26"/>
         <source>SCREENS</source>
-        <translation type="unfinished"></translation>
+        <translation>EKRANLAR</translation>
     </message>
     <message>
         <location filename="../displaypositionwidget.ui" line="33"/>
         <source>Select a screen to configure it, or drag it around to reposition it. To duplicate displays, drag them on top of each other and set them to have the same resolution.</source>
-        <translation type="unfinished"></translation>
+        <translation>Yapılandırmak için bir ekran seçin veya yeniden konumlandırmak için sürükleyin. Ekranları çoğaltmak için, bunları birbirinin üzerine sürükleyin ve aynı çözünürlüğe sahip olacak şekilde ayarlayın.</translation>
     </message>
     <message>
         <location filename="../displaypositionwidget.ui" line="78"/>
         <source>Set</source>
-        <translation type="unfinished"></translation>
+        <translation>Ayarlamak</translation>
     </message>
     <message>
         <location filename="../displaypositionwidget.cpp" line="164"/>
         <source>No Powered Displays</source>
-        <translation type="unfinished"></translation>
+        <translation>Elektrikli Ekran Yok</translation>
     </message>
     <message>
         <location filename="../displaypositionwidget.cpp" line="164"/>
         <source>All available displays in your configuration are off. Are you sure you want to set this screen configuration?</source>
-        <translation type="unfinished"></translation>
+        <translation>Yapılandırmanızdaki mevcut tüm ekranlar kapalıdır. Bu ekran yapılandırmasını ayarlamak istediğinizden emin misin?</translation>
     </message>
 </context>
 </TS>
