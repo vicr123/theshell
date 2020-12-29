87f47b3aae5e2488a5c8f5e5a5cf96ad01b97c1e
Aleyna Tilki <atahanreis@icloud.com>
2020-12-29 00:54:03 +0000

    Translated using Weblate (Turkish)
    Currently translated at 94.6% (53 of 56 strings)
    
    Translation: theShell/theShell Notifications
    Translate-URL: https://translate.vicr123.com/projects/theshell/theshell-notifications/tr/

--- statuscenter/NotificationsPane/translations/tr_TR.ts
+++ statuscenter/NotificationsPane/translations/tr_TR.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../settings/applicationnotificationmodel.cpp" line="62"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Genel</translation>
     </message>
 </context>
 <context>
@@ -14,8 +14,9 @@
     <message numerus="yes">
         <location filename="../kjob/jobserver.cpp" line="77"/>
         <source>%n jobs running</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n iş çalışıyor</numerusform>
+            <numerusform>%n iş çalışıyor</numerusform>
         </translation>
     </message>
 </context>
@@ -24,7 +25,7 @@
     <message>
         <location filename="../kjob/jobviewwidget.ui" line="14"/>
         <source>Frame</source>
-        <translation type="unfinished"></translation>
+        <translation>Çerçeve</translation>
     </message>
 </context>
 <context>
@@ -32,13 +33,14 @@
     <message>
         <location filename="../notificationsWidget/notificationappgroup.cpp" line="122"/>
         <source>Collapse Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Bildirimleri Daralt</translation>
     </message>
     <message numerus="yes">
         <location filename="../notificationsWidget/notificationappgroup.cpp" line="125"/>
         <source>+%n notifications collapsed</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>+% n bildirim daraltıldı</numerusform>
+            <numerusform>+% n bildirim daraltıldı</numerusform>
         </translation>
     </message>
 </context>
@@ -47,28 +49,31 @@
     <message numerus="yes">
         <location filename="../notificationsWidget/notificationpanel.cpp" line="54"/>
         <source>%n d</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n g</numerusform>
+            <numerusform>%n g</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../notificationsWidget/notificationpanel.cpp" line="56"/>
         <source>%n hr</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n sa</numerusform>
+            <numerusform>%n sa</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../notificationsWidget/notificationpanel.cpp" line="58"/>
         <source>%n min</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n dak</numerusform>
+            <numerusform>%n dak</numerusform>
         </translation>
     </message>
     <message>
         <location filename="../notificationsWidget/notificationpanel.cpp" line="60"/>
         <source>just now</source>
-        <translation type="unfinished"></translation>
+        <translation>şimdi</translation>
     </message>
 </context>
 <context>
@@ -76,14 +81,14 @@
     <message>
         <location filename="../notificationsWidget/notificationpopup.ui" line="280"/>
         <source>Hide</source>
-        <translation type="unfinished"></translation>
+        <translation>Sakla</translation>
     </message>
     <message>
         <location filename="../notificationsWidget/notificationpopup.ui" line="296"/>
         <location filename="../notificationsWidget/notificationpopup.cpp" line="287"/>
         <location filename="../notificationsWidget/notificationpopup.cpp" line="301"/>
         <source>Dismiss</source>
-        <translation type="unfinished"></translation>
+        <translation>Reddet</translation>
     </message>
 </context>
 <context>
@@ -92,38 +97,38 @@
         <location filename="../notificationsWidget/notificationswidget.ui" line="40"/>
         <location filename="../notificationsWidget/notificationswidget.cpp" line="110"/>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Bildirimler</translation>
     </message>
     <message>
         <location filename="../notificationsWidget/notificationswidget.ui" line="88"/>
         <source>Quiet Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Sessiz mod</translation>
     </message>
     <message>
         <location filename="../notificationsWidget/notificationswidget.ui" line="100"/>
         <source>Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>Ses</translation>
     </message>
     <message>
         <location filename="../notificationsWidget/notificationswidget.ui" line="117"/>
         <source>Critical Only</source>
-        <translation type="unfinished"></translation>
+        <translation>Yalnızca Kritik</translation>
     </message>
     <message>
         <location filename="../notificationsWidget/notificationswidget.ui" line="131"/>
         <location filename="../notificationsWidget/notificationswidget.cpp" line="81"/>
         <source>No Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Bildirim Yok</translation>
     </message>
     <message>
         <location filename="../notificationsWidget/notificationswidget.ui" line="145"/>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>Sessiz</translation>
     </message>
     <message>
         <location filename="../notificationsWidget/notificationswidget.ui" line="245"/>
         <source>Fore&amp;ver</source>
-        <translation type="unfinished"></translation>
+        <translation>Sons&amp;uza dek</translation>
     </message>
     <message>
         <location filename="../notificationsWidget/notificationswidget.ui" line="254"/>
@@ -133,7 +138,7 @@
     <message>
         <location filename="../notificationsWidget/notificationswidget.ui" line="277"/>
         <source>HH:mm</source>
-        <translation type="unfinished"></translation>
+        <translation>DD:aa</translation>
     </message>
     <message>
         <location filename="../notificationsWidget/notificationswidget.ui" line="288"/>
@@ -144,23 +149,24 @@
         <location filename="../notificationsWidget/notificationswidget.ui" line="389"/>
         <location filename="../notificationsWidget/notificationswidget.cpp" line="191"/>
         <source>No notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Bildirim yok</translation>
     </message>
     <message>
         <location filename="../notificationsWidget/notificationswidget.ui" line="399"/>
         <source>There&apos;s nothing to see at the moment. Don&apos;t worry, we&apos;ll be sure to tell you as soon as something pops up.</source>
-        <translation type="unfinished"></translation>
+        <translation>Şu anda görülecek bir şey yok. Merak etmeyin, bir şey ortaya çıkar çıkmaz size söyleyeceğimizden emin olacağız.</translation>
     </message>
     <message>
         <location filename="../notificationsWidget/notificationswidget.ui" line="447"/>
         <source>Clear All</source>
-        <translation type="unfinished"></translation>
+        <translation>Hepsini Temizle</translation>
     </message>
     <message numerus="yes">
         <location filename="../notificationsWidget/notificationswidget.cpp" line="194"/>
         <source>%n notification(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n bildirim</numerusform>
+            <numerusform>%n bildirim</numerusform>
         </translation>
     </message>
 </context>
@@ -169,98 +175,98 @@
     <message>
         <location filename="../settings/settingspane.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="95"/>
         <location filename="../settings/settingspane.cpp" line="114"/>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Bildirimler</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="157"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Genel</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="242"/>
         <source>Show Contents</source>
-        <translation type="unfinished"></translation>
+        <translation>İçeriği Göster</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="258"/>
         <source>Hide Contents</source>
-        <translation type="unfinished"></translation>
+        <translation>İçeriği Gizle</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="271"/>
         <source>Don&apos;t Show</source>
-        <translation type="unfinished"></translation>
+        <translation>Gösterme</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="300"/>
         <source>On lock screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Kilit ekranında</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="313"/>
         <source>DISPLAY</source>
-        <translation type="unfinished"></translation>
+        <translation>EKRAN</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="320"/>
         <source>Emphasise sending app</source>
-        <translation type="unfinished"></translation>
+        <translation>Uygulama göndermeyi vurgulayın</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="366"/>
         <source>Notification Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilgilendirme Sesi</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="387"/>
         <source>Triple Ping</source>
-        <translation type="unfinished"></translation>
+        <translation>Üçlü Ping</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="392"/>
         <source>Upside Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Tepe taklak</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="397"/>
         <source>Echo</source>
-        <translation type="unfinished"></translation>
+        <translation>Eko</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="405"/>
         <source>Attenuate other sounds on notification</source>
-        <translation type="unfinished"></translation>
+        <translation>Bildirimde diğer sesleri zayıflatın</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="418"/>
         <source>SOUNDS</source>
-        <translation type="unfinished"></translation>
+        <translation>SESLER</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="425"/>
         <source>Notification Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilgilendirme Sesi</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="474"/>
         <source>Charging</source>
-        <translation type="unfinished"></translation>
+        <translation>Doluyor</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="494"/>
         <source>SYSTEM NOTIFICATIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>SİSTEM BİLDİRİMLERİ</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="501"/>
         <source>Media Connection</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Bağlantısı</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="521"/>
@@ -270,59 +276,61 @@
     <message>
         <location filename="../settings/settingspane.ui" line="658"/>
         <source>Allow Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Bildirimlere İzin Ver</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="709"/>
         <source>Pop up onscreen</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekranda açılır</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="723"/>
         <source>Play Sounds</source>
-        <translation type="unfinished"></translation>
+        <translation>Sesleri Çal</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="730"/>
         <source>Bypass Quiet Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Sessiz Modu Atla</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="770"/>
         <source>Even when Quiet Mode is set to No Notifications or Critical Only, notifications from this app will continue to show</source>
-        <translation type="unfinished"></translation>
+        <translation>Sessiz Mod, Bildirim Yok veya Yalnızca Kritik olarak ayarlandığında bile, bu uygulamadan gelen bildirimler gösterilmeye devam edecek</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="786"/>
         <source>BEHAVIOUR</source>
-        <translation type="unfinished"></translation>
+        <translation>DAVRANIŞ</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="833"/>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>HAREKETLER</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="843"/>
         <source>Mark as uninstalled</source>
-        <translation type="unfinished"></translation>
+        <translation>Kaldırıldı olarak işaretle</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.ui" line="846"/>
         <source>If this application has been uninstalled, you can remove this application from the list of notifications.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu uygulama kaldırıldıysa, bu uygulamayı bildirimler listesinden kaldırabilirsiniz.</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.cpp" line="230"/>
         <source>Mark as uninstalled?</source>
-        <translation type="unfinished"></translation>
+        <translation>Kaldırıldı olarak işaretlensin mi?</translation>
     </message>
     <message>
         <location filename="../settings/settingspane.cpp" line="230"/>
         <source>This will remove the settings from theShell. If the application sends another notification, it will reappear.
 
 Mark &quot;%1&quot; as uninstalled?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu, ayarları theShell&apos;den kaldıracaktır. Uygulama başka bir bildirim gönderirse, yeniden görünecektir.
+
+&quot;%1&quot; i kaldırılmış olarak işaretle?</translation>
     </message>
 </context>
 </TS>
